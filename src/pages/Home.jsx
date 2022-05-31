a38f1ca35636d0122ce7203591cd5be73ca9f950
Nikita <nvadeveloper@gmail.com>
2022-05-31 22:27:10 +0300

    add pagination component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import PizzaBlock from '../components/PizzaBlock';
 import Placeholder from '../components/PizzaBlock/Placeholder';
 import Sort from '../components/Sort';
 
-const Home = () => {
+const Home = ({ searchValue }) => {
     const [items, setItems] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
 
@@ -15,14 +15,28 @@ const Home = () => {
         sortProperty: 'raiting',
     });
 
+    const pizzas = items
+        // .filter((obj) => {
+        //     if (obj.title.toLowerCase().includes(searchValue.toLowerCase())) {
+        //         return true;
+        //     }
+
+        //     return false;
+        // })
+        .map((obj) => <PizzaBlock {...obj} key={obj.id} />);
+    const placeholder = [...new Array(6)].map((_, i) => <Placeholder key={i} />);
+
     useEffect(() => {
         setIsLoading(true);
+
+        const sortBy = sortType.sortProperty.replace('-', '');
+        const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc';
+        const category = categoryId > 0 ? `category=${categoryId}` : '';
+        const search = searchValue ? `&search=${searchValue}` : '';
+
         fetch(
-            `https://629128b827f4ba1c65c8cf57.mockapi.io/items?${
-                categoryId > 0 ? `category=${categoryId}` : ''
-            }&sortBy=${sortType.sortProperty.replace('-', '')}&order=${
-                sortType.sortProperty.includes('-') ? 'asc' : 'desc'
-            }`,
+            `https://629128b827f4ba1c65c8cf57.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}${search}`,
+            // ${searchValue ? `&search=${searchValue}` : ''}`,
         )
             .then((res) => res.json())
             .then((arr) => {
@@ -30,7 +44,7 @@ const Home = () => {
                 setIsLoading(false);
             });
         window.scrollTo(0, 0);
-    }, [categoryId, sortType]);
+    }, [categoryId, sortType, searchValue]);
 
     return (
         <>
@@ -40,11 +54,7 @@ const Home = () => {
                     <Sort value={sortType} onChangeSort={(id) => setSortType(id)} />
                 </div>
                 <h2 className="content__title">Все пиццы</h2>
-                <div className="content__items">
-                    {isLoading
-                        ? [...new Array(6)].map((_, i) => <Placeholder key={i} />)
-                        : items.map((obj) => <PizzaBlock {...obj} key={obj.id} />)}
-                </div>
+                <div className="content__items">{isLoading ? placeholder : pizzas}</div>
             </div>
         </>
     );
