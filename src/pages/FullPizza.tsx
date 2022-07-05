5edc8c25a03983332d2ccc5c9fbce2afdcc77485
Nikita <nvadeveloper@gmail.com>
2022-07-05 21:12:15 +0300

    change jsx -> tsx in FullPizza

--- src/pages/FullPizza.tsx
+++ src/pages/FullPizza.tsx
@@ -1,4 +1,4 @@
-// import React from 'react';
+import React from 'react';
 import { useState, useEffect } from 'react';
 import { useNavigate, useParams, Link } from 'react-router-dom';
 import axios from 'axios';
