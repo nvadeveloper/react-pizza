425f756ef8d4d2a408cdf875fac009dbb452d6a9
Nikita <nvadeveloper@gmail.com>
2022-07-21 00:19:58 +0300

    change Sort items

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -2,7 +2,7 @@ import React, { useCallback, useRef, useState } from 'react';
 import debounce from 'lodash.debounce';
 import style from './Search.module.scss';
 import { useDispatch } from 'react-redux';
-import { setSearchValue } from '../../redux/slices/filterSlice.ts';
+import { setSearchValue } from '../../redux/slices/filterSlice';
 
 export const Search: React.FC = () => {
     const dispatch = useDispatch();
